2d623699032eb52415c4c6d8bd4ed4da85fa90cf
shalu-1272 <bansal.shalu19@gmail.com>
2024-07-20 21:56:25 +0530

    Commit LeapYearCode

--- src/PracticeQues_07202024/LeapYear.java
+++ src/PracticeQues_07202024/LeapYear.java
@@ -1,2 +1,24 @@
-package PracticeQues_07202024;public class LeapYear {
-}
+
+package PracticeQues_07202024;
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args)
+    {
+       //int year = 2024;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year /n");
+        int year = sc.nextInt();
+
+
+        if (((year%4)==0) && ((year%100)!=0) || ((year%400)==0))
+        {
+            System.out.println("Leap Year");
+        }
+        else {
+            System.out.println("Not a leap year");
+        }
+
+    }
+    }
+
---
+++ src/PracticeQues_07202024/TriangleClassifier.java
@@ -0,0 +1,4 @@
+package PracticeQues_07202024;
+
+public class TriangleClassifier {
+}
