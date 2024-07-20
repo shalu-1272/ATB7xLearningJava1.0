7671ab2ece1b15f75de5660fda1bc875fb4e48e2
shalu-1272 <bansal.shalu19@gmail.com>
2024-07-20 23:15:38 +0530

    Commit LeapYearCode

--- src/PracticeQues_07202024/TriangleClassifier.java
+++ src/PracticeQues_07202024/TriangleClassifier.java
@@ -1,4 +1,34 @@
 package PracticeQues_07202024;
-
+import java.util.Scanner;
 public class TriangleClassifier {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the dimensions: length , breadth and width");
+        float length = sc.nextFloat();
+        float width = sc.nextFloat();
+        float breadth = sc.nextFloat();
+
+        if (length==breadth && breadth==width)
+        {
+            System.out.println("Equilateral Triangle");
+        }
+        else if (length==breadth || length == width && width != breadth)
+        {
+            System.out.println("Isosceles Triangle");
+        }
+        else if (breadth==width || breadth == length && length != width)
+        {
+            System.out.println("Isosceles Triangle");
+        }
+        else if (width == breadth || width == length && length != breadth)
+        {
+            System.out.println("Isosceles Triangle");
+        }
+            else
+        {
+            System.out.println("Scalene Triangle");
+        }
+
+    }
 }
