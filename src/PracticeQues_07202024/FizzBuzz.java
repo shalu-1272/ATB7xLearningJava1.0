68e8cf1fd7ffeef5a2aa35582166a79cc0925406
shalu-1272 <bansal.shalu19@gmail.com>
2024-07-23 14:39:26 +0530

    commit

--- src/PracticeQues_07202024/FizzBuzz.java
+++ src/PracticeQues_07202024/FizzBuzz.java
@@ -3,30 +3,24 @@ import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter a number");
-            short number = sc.nextShort();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
 
-
-            if(number >0 && number <=100 && number%3==0 && number%5!=0)
-            {
+        for (int i = 1; i <= n ; i++)
+        {
+            if (i % 5 == 0 && i%3 == 0) {
+            System.out.println("FizzBuzz");
+        }
+           else if (i % 3 == 0 && i % 5 != 0) {
                 System.out.println("Fizz");
             }
-            else if (number >0 && number <=100 && number%5==0 && number%3!=0)
-            {
+           else if (i % 5 == 0 && i%3!=0) {
                 System.out.println("Buzz");
             }
-            else if (number >0 && number <=100 && number%5==0 && number%3==0)
-            {
-                System.out.println("FizzBuzz");
-            }
-            else if (number > 100 && number % 5 == 0 || number % 3 == 0)
-            {
-                System.out.println("Out of 1-100 Range");
-            }
-            else
-            {
-                System.out.println("Neither a multiple of 3 nor 5");
-            }
+           else{
+            System.out.println(i);
         }
         }
+    }
+}
