fc4a9cc22ec19f5fda5a7ee01d029b144c2b2ec0
shalu-1272 <bansal.shalu19@gmail.com>
2024-07-24 11:50:32 +0530

    Practice Quest Left and Right Star Pattern

---
+++ src/PracticeQues_07202024/LeftPatterStarProgram.java
@@ -0,0 +1,16 @@
+package PracticeQues_07202024;
+
+public class LeftPatterStarProgram {
+    public static void main(String[] args) {
+        for(int i=1; i<=5;i++)
+        {
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+
+            }
+
+    }
+}
