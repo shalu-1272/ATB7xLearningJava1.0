490b7c6c05c4f7d232ed53b6a628f00459ea5c21
shalu-1272 <bansal.shalu19@gmail.com>
2024-07-27 10:06:34 +0530

    Lab exercises

--- src/ex_07212024/Lab096.java
+++ src/ex_07212024/Lab096.java
@@ -3,6 +3,17 @@ package ex_07212024;
 public class Lab096 {
     public static void main(String[] args) {
         {
+//loop = while and do while. very similar to for
+            //while - A,B,C
+            //A- INITIALIZATION
+            //B= CONDITION
+            //INCREMENT/DECREMENT
+            int i=0;  //A
+            while(i<=10) //B
+            {
+                System.out.println(i);
+                i++; //C
+            }
 
         }
     }
